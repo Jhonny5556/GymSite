39973f955ad1674802185358e763455561afb078
Joseph <59311462+JosephianDev@users.noreply.github.com>
2025-04-03 16:10:52 +0200

    Modifiche generali
    - Aggiunta dei file Termini di servizio e Politica della Privacy
    - Aggiornato il footer
    - Aggiornato lo shop
    - Aggiornata la pagina home
    - Creata la pagina di Contattaci

--- js/footer.js
+++ js/footer.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const footer = document.createElement("footer");
     footer.innerHTML = `
         <div class="footer-content">
-            <p>&copy; 2025 Praise the bar</p>
+            <p>&copy; 2025 PowerHouse Equipment</p>
             <p>All rights reserved</p>
         </div>
         <ul class="footer-links">
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms of Service</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <li><a href="privacy_policy.htm">Privacy Policy</a></li>
+            <li><a href="term_of_service.htm">Terms of Service</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
         </ul>
     `;
     body.appendChild(footer);
